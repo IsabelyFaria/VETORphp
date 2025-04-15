3ae3f07456d664ad7bfdd9d394ece6e1dd1dcbf1
GeovanaCunha <bellyfaria04@gmail.com>
2025-04-15 10:03:10 -0300

    teste de comit e feito exercicio 1 (TESTADO)

--- exercicio1/calcular.php
+++ exercicio1/calcular.php
@@ -1,8 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 1 - Gerar um Número Aleatório</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<div class="container">
+    <?php
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['numero'])) {
         $randomNumber = rand(0, 99);
         echo "<p>Número gerado: <strong>$randomNumber</strong></p>";
     }
     ?>
+    
     <form action="index.php" method="post">
     <button type="submit">Voltar</button>
+    </div>
+
+</body>
+</html>
--- exercicio1/index.php
+++ exercicio1/index.php
@@ -8,10 +8,12 @@
 </head>
 <body>
 
-    <h1>Gerador de Número Aleatório</h1>
+    <div class="container">
+    <h1>Gerador de Números Aleatórios</h1>
     <form action="calcular.php" method="post">
-    <button type="submit" name="numero">Gerar Número</button>
+        <button type="submit" name="numero">Gerar Número</button>
     </form>
+  </div>
 
 </body>
 </html>

432e8eab0a8f9499d74974cbe94559dc3c749c32
GeovanaCunha <geovanahcunha1109@gmail.com>
2025-04-15 09:33:59 -0300

    adicionando o css EXERCICIO 1

--- exercicio1/index.php
+++ exercicio1/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio 1 - Gerar um Número Aleatório</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
